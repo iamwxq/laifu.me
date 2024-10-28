b3f4008810134774d440cd8ae5528084ab6b76c6
iamwxq <whoeverimf5@gmail.com>
2024-10-28 15:12:11 +0800

    feat(repo): first commit

---
+++ app/routes/_index.tsx
@@ -0,0 +1,15 @@
+function Index() {
+  return (
+    <div className="min-h-screen w-screen">
+      <div className="mx-auto max-w-[1920px]">
+        <header>Header</header>
+
+        <main>Body</main>
+
+        <footer>Footer</footer>
+      </div>
+    </div>
+  );
+};
+
+export default Index;
---
+++ eslint.config.js
@@ -0,0 +1,46 @@
+import antfu from "@antfu/eslint-config";
+import { FlatCompat } from "@eslint/eslintrc";
+
+const compat = new FlatCompat();
+
+export default antfu(
+  {
+    stylistic: {
+      jsx: true, // react
+      indent: 2,
+      semi: true,
+      quotes: "double",
+    },
+    react: true, // react
+    rules: {
+      "style/quotes": ["error", "double"],
+    },
+  },
+  // react
+  {
+    rules: {
+      "react/prop-types": "off",
+      "react-refresh/only-export-components": "off",
+      "style/jsx-one-expression-per-line": "off",
+      "style/jsx-sort-props": [
+        "error",
+        {
+          ignoreCase: false,
+          callbacksLast: true,
+          reservedFirst: true,
+          shorthandFirst: true,
+          shorthandLast: false,
+          noSortAlphabetically: false,
+          multiline: "last",
+        },
+      ],
+    },
+  },
+  ...compat.config({
+    // https://github.com/francoismassart/eslint-plugin-tailwindcss
+    extends: ["plugin:tailwindcss/recommended"],
+    rules: {
+      "tailwindcss/no-custom-classname": "off",
+    },
+  }),
+);
