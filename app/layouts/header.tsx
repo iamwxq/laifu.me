3e174fe52b1eb23cac2f394b47cabd475231e118
iamwxq <whoeverimf5@gmail.com>
2024-11-15 17:39:28 +0800

    wip

--- app/layouts/header.tsx
+++ app/layouts/header.tsx
@@ -1,11 +1,13 @@
 import { NavLink, useLocation } from "@remix-run/react";
 import clsx from "clsx";
 import { Moon, Sun } from "lucide-react";
+import { useEffect, useState } from "react";
 import { Theme, useTheme } from "remix-themes";
 
 function Header() {
   const location = useLocation();
   const [theme, setTheme] = useTheme();
+  const [mounted, setMounted] = useState(false);
 
   function handleSwitchMode() {
     switch (theme) {
@@ -16,6 +18,10 @@ function Header() {
         setTheme(Theme.DARK);
     }
   }
+
+  useEffect(() => {
+    setMounted(true);
+  }, []);
 
   return (
     <header className={clsx(
@@ -82,15 +88,25 @@ function Header() {
           type="button"
           onClick={handleSwitchMode}
         >
-          <Sun
-            className="size-5 cursor-pointer select-none text-zinc-200 transition-all group-hover:text-zinc-50"
-            style={{ display: theme === Theme.LIGHT ? "none" : "block" }}
-          />
+          {!mounted && <div className="size-5" />}
 
-          <Moon
-            className="size-5 cursor-pointer select-none text-zinc-500 transition-all group-hover:text-zinc-800"
-            style={{ display: theme === Theme.DARK ? "none" : "block" }}
-          />
+          {mounted && (
+            <Sun
+              className={clsx(
+                "size-5 cursor-pointer select-none text-zinc-200 transition-all group-hover:text-zinc-50",
+                theme === Theme.LIGHT && "hidden",
+              )}
+            />
+          )}
+
+          {mounted && (
+            <Moon
+              className={clsx(
+                "size-5 cursor-pointer select-none text-zinc-500 transition-all group-hover:text-zinc-800",
+                theme === Theme.DARK && "hidden",
+              )}
+            />
+          )}
         </button>
       </div>
     </header>
